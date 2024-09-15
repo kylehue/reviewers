3ab322f44f3264d82ff51ab14cf38dc733fe616e
kylehue <apeuda69@gmail.com>
2024-09-15 19:10:33 +0800

    feat(scramble): shuffle lists

--- .vitepress/theme/components/scrambler.vue
+++ .vitepress/theme/components/scrambler.vue
@@ -11,67 +11,95 @@ import { PhShuffle, PhRepeat } from "@phosphor-icons/vue";
 import Button from "./button.vue";
 import { shuffleArray } from "../utils/common";
 
-const props = defineProps<{
-   tag: string;
-}>();
+const props = withDefaults(
+   defineProps<{
+      tag: keyof HTMLElementTagNameMap;
+      includeLists?: boolean;
+   }>(),
+   {
+      includeLists: () => true,
+   }
+);
 
 let isScrambled = ref(false);
-let contentParents: HTMLElement[] = [];
-let content: HTMLElement[][] = [];
+let sectionContentParents: HTMLElement[] = [];
+let sectionContent: HTMLElement[][] = [];
+let listContentParents: HTMLElement[] = [];
+let listContent: HTMLElement[][] = [];
 
 const toggleScramble = () => {
    if (!isScrambled.value) {
-      scrambleSections();
+      scramble();
    } else {
-      restoreOriginalOrder();
+      unscramble();
    }
    isScrambled.value = !isScrambled.value;
 };
 
 function removeContent() {
-   for (let i = 0; i < content.length; i++) {
-      for (let j = 0; j < content[i].length; j++) {
-         content[i][j].remove();
+   for (let i = 0; i < sectionContent.length; i++) {
+      for (let j = 0; j < sectionContent[i].length; j++) {
+         sectionContent[i][j].remove();
       }
    }
 }
 
-function scrambleSections() {
+function scramble() {
    removeContent();
-   let randomized = shuffleArray(Array.from(content.keys()));
-   for (let index of randomized) {
-      for (let j = 0; j < content[index].length; j++) {
-         contentParents[index].appendChild(content[index][j]);
+   for (let i of shuffleArray(Array.from(sectionContent.keys()))) {
+      for (let j = 0; j < sectionContent[i].length; j++) {
+         sectionContentParents[i].appendChild(sectionContent[i][j]);
+      }
+   }
+   for (let i of shuffleArray(Array.from(listContent.keys()))) {
+      let shuffledJ = shuffleArray(Array.from(listContent[i].keys()));
+      for (let j of shuffledJ) {
+         listContentParents[i].appendChild(listContent[i][j]);
       }
    }
 }
 
-// Function to restore the original order (reloading page content)
-function restoreOriginalOrder() {
+function unscramble() {
    removeContent();
-   for (let i = 0; i < content.length; i++) {
-      for (let j = 0; j < content[i].length; j++) {
-         contentParents[i].appendChild(content[i][j]);
+   for (let i = 0; i < sectionContent.length; i++) {
+      for (let j = 0; j < sectionContent[i].length; j++) {
+         sectionContentParents[i].appendChild(sectionContent[i][j]);
+      }
+   }
+   for (let i = 0; i < listContent.length; i++) {
+      for (let j = 0; j < listContent[i].length; j++) {
+         listContentParents[i].appendChild(listContent[i][j]);
       }
    }
 }
 
 function initContent() {
-   let ungroupedContent = Array.from(
-      document.querySelectorAll<HTMLElement>(props.tag)
+   let rawSections = Array.from(
+      document.querySelectorAll<HTMLElement>(`.vp-doc ${props.tag}`)
    );
-   for (let i = 0; i < ungroupedContent.length; i++) {
-      let currentGroup: HTMLElement[] = [ungroupedContent[i]];
-      let currentEl = ungroupedContent[i];
+   for (let i = 0; i < rawSections.length; i++) {
+      let currentGroup: HTMLElement[] = [rawSections[i]];
+      let currentEl = rawSections[i];
       while (
          currentEl.nextElementSibling !== null &&
-         currentEl.nextElementSibling !== ungroupedContent[i + 1]
+         currentEl.nextElementSibling !== rawSections[i + 1]
       ) {
          currentGroup.push(currentEl.nextElementSibling as HTMLElement);
          currentEl = currentEl.nextElementSibling as HTMLElement;
       }
-      content.push(currentGroup);
-      contentParents.push(currentGroup[0].parentElement!);
+      sectionContent.push(currentGroup);
+      sectionContentParents.push(currentGroup[0].parentElement!);
+   }
+
+   if (props.includeLists) {
+      let rawLists = Array.from(
+         document.querySelectorAll<HTMLElement>(".vp-doc ul")
+      );
+      for (let i = 0; i < rawLists.length; i++) {
+         let currentGroup = Array.from(rawLists[i].children) as HTMLElement[];
+         listContent.push(currentGroup);
+         listContentParents.push(rawLists[i]);
+      }
    }
 }
 
--- .vitepress/theme/utils/common.ts
+++ .vitepress/theme/utils/common.ts
@@ -1,6 +1,6 @@
 export function shuffleArray(arr: any[]) {
-   for (let i = arr.length - 1; i >= 0; i--) {
-      let j = ~~(Math.random() * arr.length);
+   for (let i = arr.length - 1; i > 0; i--) {
+      let j = ~~(Math.random() * (i + 1));
       [arr[i], arr[j]] = [arr[j], arr[i]];
    }
    return arr;
